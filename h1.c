937868fb711fa6ea1b898315b645ef2b40fd228c
darkalx12 <darkword907@gmail.com>
2023-09-06 07:00:59 -0700

    simple-shell'

---
+++ h1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- add the numbers
+ *
+ * Return: retun 0
+*/
+int main(void)
+{
+int age = 2;
+int gear = 3;
+int sum = age + gear;
+printf("there age is %d\n", sum);
+return (0);
+}
